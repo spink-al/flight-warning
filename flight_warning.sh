076e0c04428f5e9e9aef49bec8be1755e3a1c5bb
Spinki <23009891+spink-al@users.noreply.github.com>
2020-02-26 11:07:34 +0100

    Update flight_warning.sh

--- flight_warning.sh
+++ flight_warning.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true ; do
-    # localhost or ip
+    # localhost or ip with merged feed from VRS
     nc -w 30 localhost 33333 | python flight_warning_MLAT.py
     #nc -w 30 localhost 33333 | python3 flight_warning_MLAT.py
     sleep 5s 
