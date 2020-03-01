4628f9004a350586f39db45b211f9b46f4f4f4cf
Spinki <23009891+spink-al@users.noreply.github.com>
2020-03-01 12:48:10 +0100

    output to file field

--- flight_warning_Conf.py
+++ flight_warning_Conf.py
@@ -1,3 +1,14 @@
+# Lat/Lon
+MY_LAT="51.1234"
+MY_LON="15.1234"
+# Meters above sea level
+MY_ALT="90"
+
+# data output to file, for no output change "/tmp/out.txt" to "/dev/null"
+out_path="/tmp/out.txt"
+
+
+### not used
 BIN_FLDR="/home/pi/AllSkyRadar"
 VRS_FLDR="/home/pi/VRS"
 FW_FLDR="/home/pi/flight_warning"
@@ -6,6 +17,4 @@ MISC_FLDR="/home/pi/AllSkyRadar/misc"
 CONF_FLDR="/home/pi/AllSkyRadar/config"
 STORAGE_IP="192.168.3.112"
 STORAGE_FLDR="/home/pi/work/arch/AS/_ASI"
-MY_LAT="51.1234"
-MY_LON="15.1234"
-MY_ALT="90"
+####
